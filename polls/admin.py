463826bd214a9343660d3b7ad66783ab44093d60
Astha Vairat <aav7237@nyu.edu>
2026-03-11 13:57:27 -0400

    Final cleanup for CI/CD pipelinecd

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Question, Choice
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -17,4 +15,4 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 
-admin.site.register(Question,QuestionAdmin )
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
 from .models import Question
 
+
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {
-        'latest_question_list': latest_question_list,
+        "latest_question_list": latest_question_list,
     }
-    return render(request, 'polls/index.html', context)
+    return render(request, "polls/index.html", context)
